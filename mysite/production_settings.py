18ec377feb5871059b88d52b4d501a6a8f6d9732
nightted <h5904098@gmail.com>
2016-09-25 01:18:20 +0800

    c1

--- mysite/production_settings.py
+++ mysite/production_settings.py
@@ -13,7 +13,7 @@ STATIC_ROOT = 'static'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS =  ['locahost']
 
 # Turn off DEBUG mode
 DEBUG = False
