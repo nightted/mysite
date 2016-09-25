99c9788a62678db79f0712e77e11dcba9f9be3ef
nightted <h5904098@gmail.com>
2016-09-25 16:10:07 +0800

    c1

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,8 +126,8 @@ USE_TZ = False
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
-
+MEDIA_ROOT = ''
+MEDIA_URL = '../pic/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
